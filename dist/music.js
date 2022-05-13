6c8134421a53cbf09d7eb891b38c3e444bfb01af
immHopper <759595747@qq.com>
2022-05-13 21:07:41 +0800

    Site updated: 2022-05-13 21:07:40

---
+++ dist/music.js
@@ -0,0 +1,38 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "I See Fire",
+        artist: 'Kygo Remix',
+        url: 'http://www.ytmp3.cn/down/74940.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+      },
+	  {
+        name: 'Sing Me to Sleep',
+        artist: 'Alan Walker',
+        url: 'http://www.ytmp3.cn/down/76929.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: 'Bittersweet Chocolate',
+        artist: 'mr. hong',
+        url: 'http://www.ytmp3.cn/down/77111.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: 'Dont You Worry Child',
+        artist: 'Charming Horses Remix',
+        url: 'http://music.163.com/song/media/outer/url?id=403710479.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: '阿拉斯加海湾',
+        artist: '蓝心羽',
+        url: 'http://music.163.com/song/media/outer/url?id=1500569811.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+    ]
+});
+
