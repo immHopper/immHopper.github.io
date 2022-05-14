8cfd6dc3571f98bb604cc72261144fc00bf36284
immHopper <759595747@qq.com>
2022-05-15 00:05:20 +0800

    Site updated: 2022-05-15 00:05:19

--- dist/music.js
+++ dist/music.js
@@ -3,24 +3,6 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-	{
-        name: "I See Fire",
-        artist: 'Kygo Remix',
-        url: 'http://www.ytmp3.cn/down/74940.mp3',
-        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
-      },
-	  {
-        name: 'Sing Me to Sleep',
-        artist: 'Alan Walker',
-        url: 'http://www.ytmp3.cn/down/76929.mp3',
-        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
-      },
-      {
-        name: 'Bittersweet Chocolate',
-        artist: 'mr. hong',
-        url: 'http://www.ytmp3.cn/down/77111.mp3',
-        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
-      },
       {
         name: 'Dont You Worry Child',
         artist: 'Charming Horses Remix',
@@ -33,6 +15,21 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=1500569811.mp3',
         cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
       },
+      {
+        name: 'But U',
+        artist: 'NINEONE',
+        url: 'http://music.163.com/song/media/outer/url?id=1459232593.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: 'So Long',
+        artist: 'Slushii / Madi',
+        url: 'http://music.163.com/song/media/outer/url?id=423227301.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
     ]
 });
+
+
+
 
